e72e1eb2d9c2b20f21b03a29ed2f270b446cedd2
Erica Hashert <ericahashert@gmail.com>
2022-08-10 17:04:16 -0600

    commit tree 209dbd721dd5e1047f4f22a60d152b2607c5225e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat () {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat () {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift()
+}
+function appendCat() {
+    const allCats = [...cats, "Broom"]
+return allCats
+}
+function prependCat() {
+    const allCats = ["Arnold", ...cats]
+
+return allCats
+}
+function removeLastCat() {
+   const newCats = cats.slice(0,2)
+return newCats
+}
+function removeFirstCat () {
+    const newCats = cats.slice(1)
+return newCats;
+}
